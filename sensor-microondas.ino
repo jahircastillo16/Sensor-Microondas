dd7b3fb1405b459393f31f52c551b5dd5e97dd2c
Jahir Castillo <82299102+rahajcas@users.noreply.github.com>
2023-10-22 10:25:50 -0500

    Update sensor-microondas.ino
    apaga el aire despues de 10 minutos sin detectar movimiento, falta arreglar que mande una sola señal de encendido o apagado

--- sensor-microondas.ino
+++ sensor-microondas.ino
@@ -1,29 +1,32 @@
-int ledPin   = 5;               // Pin where the LED is hooked up
-int inputPin = 19;               // Pin where teh Radar Sensor is hooked up
-int motion   = 0;               // State of motion
-int bandera_Mov=0;
+int ledPin = 5;               // Pin donde está conectado el LED
+int inputPin = 9;             // Pin donde está conectado el sensor de radar
+int motion = 0;               // Estado de movimiento
+unsigned long tiempoSinMovimiento = 0;  // Variable para rastrear el tiempo sin movimiento
+bool flag_Mov = false;        // Bandera de movimiento
 
 void setup() {
-  pinMode(ledPin, OUTPUT);      // Declare the LED pin as output
-  pinMode(inputPin, INPUT);     // Declare the sensor pin as input 
+  pinMode(ledPin, OUTPUT);    // Declarar el pin LED como salida
+  pinMode(inputPin, INPUT);   // Declarar el pin del sensor como entrada
   Serial.begin(9600);
-};
-
+}
 
 void loop() {
-  motion = digitalRead(inputPin);  // Read the input pin
-  
-  if (motion == HIGH && bandera_Mov==0) { 
-    digitalWrite(ledPin, HIGH);   // Turn on the LED
-    bandera_Mov=1;
-    Serial.println("Motion Detected!");
+  motion = digitalRead(inputPin);  // Leer el pin de entrada
+
+  if (motion == HIGH) {
+    digitalWrite(ledPin, HIGH);    // Encender el LED
+    flag_Mov = true;
+    Serial.println("¡Movimiento detectado!");
+    tiempoSinMovimiento = millis(); // Restablecer el temporizador si hay movimiento
     delay(4000);
-  } else if (motion == LOW && bandera_Mov==1) {
-    digitalWrite(ledPin, LOW && bandera_Mov==1);    // Turn off the LED
-    Serial.println("No Motion Detected!");
-    bandera_Mov=0;
+  } else if (flag_Mov && millis() - tiempoSinMovimiento > 600000) { // 600000 ms = 10 minutos
+    digitalWrite(ledPin, LOW);     // Apagar el LED después de 10 minutos sin movimiento
+    Serial.println("No se detectó movimiento durante 10 minutos.");
+    Serial.println(tiempoSinMovimiento);
+    flag_Mov = false;
+    tiempoSinMovimiento = 0;  // Reiniciar el tiempo de inactividad
     delay(4000);
-  };
+  }
 
   delay(1000);
-};
+}
